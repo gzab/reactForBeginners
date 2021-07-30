16b9d071f1c0bfb2dad9dad9af6ab026665545f4
Goncalo Florencio <g.lsb.florencio@gmail.com>
2021-07-30 16:04:46 +0200

    Added component with state update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import React, { useState } from 'react'
 import './App.css';
+import ReactDOM from 'react-dom'
 
 function App() {
     const message = "Hello!";
-    const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0)
 
     return (
     <div className="App">
@@ -17,6 +18,10 @@ function App() {
           message === 'Hello!' ? 'The message was "Hello!"' : message
         }
         <WelcomeMessage />
+        <div>
+          <p>You clicked {count} times</p>
+          <button onClick={() => setCount(count + 1)}>Click me</button>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -32,6 +37,21 @@ function App() {
 
 function WelcomeMessage() {
   return <p>Welcome!</p>
+}
+
+function CountState () {
+  const Counter = () => {
+    const [count, setCount] = useState(0)
+
+    return (
+      <div>
+        <p>You clicked {count} times</p>
+        <button onClick={() => setCount(count + 1)}>Click me</button>
+      </div>
+    )
+  }
+
+  ReactDOM.render(<Counter />, document.getElementById('app'))
 }
 
 export default App;
